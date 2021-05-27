bf01d6c20d8f0c72c15ebb8dc8bafdf588c10d17
Avineesh Kompella <AvineeshKom@gmail.com>
2021-05-26 23:13:08 -0700

    coin behavior. sure hope this dont break nothin

---
+++ Assets/CoinBehavior.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinBehavior : MonoBehaviour
+{
+
+    public GameObject player;
+    //float gravScale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //gravScale = player.GetComponent<Rigidbody2D>().gravityScale;
+        Debug.Log("Gravity scale: " + player.GetComponent<Rigidbody2D>().gravityScale);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(gravScale);
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+          //  gravScale += 9.8f;
+            player.GetComponent<Rigidbody2D>().gravityScale += 1.2f;
+            Debug.Log("Gravity scale: " + player.GetComponent<Rigidbody2D>().gravityScale);
+            Destroy(this.gameObject);
+        }
+
+    }
+}
