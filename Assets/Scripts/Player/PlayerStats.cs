9a3bed26de9adea744282a8444ea6b93fb9bc142
Evan Cheng <dr.frolicks@gmail.com>
2021-05-26 23:17:50 -0700

    added PlayerScripts file but its empty also some extra log files that shouldve been ignored

---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Stores player stats such as gold. 
+/// </summary>
+public class PlayerStats : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
