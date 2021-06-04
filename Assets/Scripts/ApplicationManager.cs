588eff1453dff1bc75ccc3839ddfce887a7027c3
Evan Cheng <dr.frolicks@gmail.com>
2021-06-03 22:20:16 -0700

    pausing adding in game

--- Assets/Scripts/ApplicationManager.cs
+++ Assets/Scripts/ApplicationManager.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class ApplicationManager : MonoBehaviour
 {
+    public GameObject pausedDisplay; 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Cancel"))
+        if (Input.GetButtonDown("Cancel"))
         {
             Time.timeScale = Time.timeScale == 1f ? 0 : 1;
+            pausedDisplay.SetActive(Time.timeScale == 0);
         }
     }
 }
